223ecf1c3960a4ccd877c298560f292b468ecfe9
Slavlned <slavlendstudio@gmail.com>
2025-04-19 18:29:33 +0500

    frames

--- src/vm/frames.rs
+++ src/vm/frames.rs
@@ -1,84 +1,98 @@
-﻿use std::collections::BTreeMap;
+﻿use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::sync::Arc;
 use crate::errors::{Error, ErrorType};
 use crate::lexer::address::Address;
 use crate::vm::values::Value;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Frame {
     map: BTreeMap<String, Value>,
-    root: Box<Option<Frame>>,
-    closure: Box<Option<Frame>>
+    root: Option<Arc<RefCell<Frame>>>,
+    closure: Option<Arc<RefCell<Frame>>>,
 }
 
 impl Frame {
     pub fn new() -> Frame {
         Frame {
             map: BTreeMap::new(),
-            root: Box::new(Option::None),
-            closure: Box::new(Option::None)
+            root: Option::None,
+            closure: Option::None
         }
     }
 
-    pub fn has(&mut self, name: String) -> bool {
-        false
-    }
-
-    pub fn exists(&mut self, name: String) -> bool{
+    pub fn has(&self, name: String) -> bool {
         if self.map.contains_key(&name) {
             true
         } else {
-            self.get_closure().has(name.clone())
+            let mut current = self.root.clone();
+            while current.is_some() {
+                current = self.root.clone();
+                if let Some(ref current) = current {
+                    if current.borrow().has(name.clone()) {
+                        return true
+                    }
+                }
+            }
+            false
         }
     }
 
-    pub fn get_closure(&mut self) -> Option<&mut Frame> {
-        self.closure.as_mut().as_mut()
-    }
-
-    pub fn lookup(&mut self, address: Address, name: String) -> Result<Value, Error> {
-        let mut current = self;
-        while !current.exists(name.clone()) {
-            match current.root.as_mut() {
-                Some(ref mut r) => {
-                    current = r;
-                }
-                None => return Err(Error::new(
-                    ErrorType::Runtime,
-                    address.clone(),
-                    format!("not found: {:?}", name.clone()),
-                    "check for var existence.".to_string()
-                ))
+    pub fn lookup(&self, address: Address, name: String) -> Result<Value, Error> {
+        // checking current frame
+        if self.map.contains_key(&name) {
+            return Ok(self.map.get(&name).unwrap().clone())
+        } else {
+            if let Some(ref closure) = self.closure {
+                return Ok(closure.borrow().lookup(address.clone(), name.clone())?)
             }
         }
-        if current.map.contains_key(&name) {
-            Ok(current.map.get(&name).unwrap().clone())
-        } else {
-            Ok(current.get_closure().unwrap().lookup(address, name)?)
+        // checking others
+        let mut current = self.root.clone();
+        while current.is_some() {
+            current = self.root.clone();
+            if let Some(ref current) = current {
+                if current.borrow().has(name.clone()) {
+                    return Ok(current.borrow().lookup(address.clone(), name.clone())?)
+                }
+            }
         }
+        // error
+        Err(Error::new(
+            ErrorType::Runtime,
+            address,
+            format!("not found: {:?}", name),
+            "check variable existence.".to_string()
+        ))
     }
 
     pub fn set(&mut self, address: Address, name: String, val: Value) -> Result<(), Error> {
-        let mut current = self;
-        while !current.exists(name.clone()) {
-            match current.root.as_mut() {
-                Some(ref mut r) => {
-                    current = r;
-                }
-                None => return Err(Error::new(
-                    ErrorType::Runtime,
-                    address.clone(),
-                    format!("not found: {:?}", name.clone()),
-                    "check for var existence.".to_string()
-                ))
+        // checking current frame
+        if self.map.contains_key(&name) {
+            self.map.insert(name, val);
+            return Ok(())
+        } else {
+            if let Some(ref mut closure) = self.closure {
+                closure.borrow_mut().map.insert(name, val);
+                return Ok(())
             }
         }
-        if current.map.contains_key(&(name.clone())) {
-            current.map.insert(name, val);
+        // checking others
+        let mut current = self.root.clone();
+        while current.is_some() {
+            current = self.root.clone();
+            if let Some(ref mut current) = current {
+                if current.borrow().has(name.clone()) {
+                    current.borrow_mut().map.insert(name.clone(), val.clone());
+                }
+            }
         }
-        Ok(())
-    }
-
-    pub fn force_set(&mut self, address: Address, name: String, val: Value) {
-        self.map.insert(name, val);
+        // error
+        Err(Error::new(
+            ErrorType::Runtime,
+            address,
+            format!("not found: {:?}", name),
+            "check variable existence.".to_string()
+        ))
     }
 }
