8483982558494b0b8de8c53061dd472a1ad84a9d
NDRAEY <andrejpavlenko666@gmail.com>
2025-06-23 23:53:40 +0900

    Brief comments

--- tools/tester/src/main.rs
+++ tools/tester/src/main.rs
@@ -1,14 +1,22 @@
 use std::{collections::HashMap, path::PathBuf};
 
+/// Данный тип описывает таблицу тестов, которые могут или не могут иметь данные для сравнения
+/// Ключ: путь к тесту
+/// Значение: Путь к данным для сравнения
 type TestMap = HashMap<String, Option<String>>;
 
+/// Результаты запуска теста
 #[derive(Default)]
 struct TesterStats {
+	/// Количество запущенных тестов
     ran: usize,
+    /// Количество успешных тестов
     ok: usize,
+    /// Количество проваленных тестов
     fail: usize
 }
 
+/// Находит все тесты, опираясь на путь `on` и возвращает список путей данных тестов.
 fn find_tests(on: &str) -> std::io::Result<Vec<PathBuf>> {
     Ok(std::fs::read_dir(on)?
         .map(|x| x.unwrap().path())
@@ -16,6 +24,8 @@ fn find_tests(on: &str) -> std::io::Result<Vec<PathBuf>> {
         .collect())
 }
 
+/// Собирает список тестов и ищет их данные для сравнения
+/// Если есть данные для теста, то значение становится `Some(<путь к данным>)`
 fn build_verification_table(on: &str) -> std::io::Result<TestMap> {
     let tests = find_tests(on)?;
     let outputs_path = on.to_string() + "/output/";
@@ -39,15 +49,20 @@ fn build_verification_table(on: &str) -> std::io::Result<TestMap> {
     Ok(table)
 }
 
+/// Запускает все тесты из таблицы
 fn run_tests(watt_path: &str, tests_table: &HashMap<String, Option<String>>) -> TesterStats {
     let mut stats = TesterStats::default();
 
     for (test_file, expected_content_file) in tests_table {
+    	// Создаём команду для запуска Watt
         let mut command = std::process::Command::new(watt_path);
+        // Добавляем аргумент пути файла для запуска
         let command = command.arg(test_file);
 
         match command.output() {
+        	// Если программа была запущена...
             Ok(data) => {
+            	// ...неуспешно, то это провал
                 if !data.status.success() {
                     println!("[FAIL] {test_file}");
                     
@@ -57,6 +72,7 @@ fn run_tests(watt_path: &str, tests_table: &HashMap<String, Option<String>>) -> 
                     continue;
                 }
 
+				// Если нет ожидаемых данных, то это хорошо, поскольку программа была обработана нормально
                 if expected_content_file.is_none() {
                     println!("[NO OUTPUT - OK] {test_file}");
 
@@ -65,10 +81,12 @@ fn run_tests(watt_path: &str, tests_table: &HashMap<String, Option<String>>) -> 
                     let verify_data = std::fs::read(expected_content_file.as_ref().unwrap()).unwrap();
 
                     if verify_data == data.stdout {
+                    	// Если даннве совпали, то тест пройден.
                         println!("[OK] {test_file}");
 
                         stats.ok += 1;
                     } else {
+                        // Ну а если нет, то печатаем отчет об ощибке.
                         println!("[FAIL] {test_file}");
                         println!("Expected:\n---");
 
@@ -85,6 +103,8 @@ fn run_tests(watt_path: &str, tests_table: &HashMap<String, Option<String>>) -> 
                 }
             },
             Err(err) => {
+            	// Если Watt не удалось запустить, то это ошибка
+            	// TODO: Возможно если Watt не удастся запустить то все тесты будут считаться проваленными, поэтому необходимо остановить процесс тестирования.
                 println!("[ERR] {test_file}: {err:?}");
 
                 stats.fail += 1;
@@ -100,6 +120,7 @@ fn run_tests(watt_path: &str, tests_table: &HashMap<String, Option<String>>) -> 
 }
 
 fn main() {
+	// Первым аргументом программы является путь к корню исходного кода. (где хранятся исходники).
     let working_directory = match std::env::args().nth(1) {
         Some(path) => path,
         None => {
@@ -114,6 +135,7 @@ fn main() {
     // println!("{:?}", compiler_path);
     // println!("{:?}", tests_path);
 
+	// Переходим к рабочей директории.
     std::env::set_current_dir(&working_directory).unwrap();
 
     if !std::fs::exists(&compiler_path).unwrap_or(false) {
@@ -123,6 +145,7 @@ fn main() {
         std::process::exit(1);
     }
 
+	// Собираем данные о тестах.
     let tests_table = match build_verification_table(&tests_path) {
         Ok(data) => data,
         Err(e) => {
@@ -131,6 +154,7 @@ fn main() {
         }
     };
 
+	// Запускаем тесты.
     let stats = run_tests(&compiler_path, &tests_table);
 
     println!("test results: {} ran: {} ok, {} fail", stats.ran, stats.ok, stats.fail);
