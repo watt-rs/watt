16a4316a3b1635dabe267c0e66c7594ee6ee1835
Andrey <pikachu_andrey@vk.com>
2025-06-24 15:20:18 +0900

    Silent compiler

--- tools/w.sh
+++ tools/w.sh
@@ -4,14 +4,14 @@ DIR=$(dirname $(realpath $0))
 
 if [[ $1 == "test" ]]; then
 	cd $DIR/tester
-	cargo r --release test $DIR/..
+	cargo r -q --release test $DIR/..
 elif [[ $1 == "bench" ]]; then
 	if [[ -z $2 ]]; then
 		echo "Select .wt file to bench"
 		exit 1
 	fi
 	cd $DIR/tester
-	cargo r --release bench $DIR/.. $2
+	cargo r -q --release bench $DIR/.. $2
 elif [[ $1 == "help" ]]; then
 	echo "Usage: $0 mode [FILES...]"
 	echo "Available modes: test, bench"
