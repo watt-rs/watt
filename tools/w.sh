dfebdb2f5d4b6383e3526f621cf3a65354c44e81
Andrey <pikachu_andrey@vk.com>
2025-07-04 20:50:04 +0900

    Compile Watt automatically in test; +1 test

--- tools/w.sh
+++ tools/w.sh
@@ -3,14 +3,14 @@
 DIR=$(dirname $(realpath $0))
 
 if [[ $1 == "test" ]]; then
-	cd $DIR/tester
+	cd $DIR/test_runner
 	cargo r -q --release test $DIR/..
 elif [[ $1 == "bench" ]]; then
 	if [[ -z $2 ]]; then
 		echo "Select .wt file to bench"
 		exit 1
 	fi
-	cd $DIR/tester
+	cd $DIR/test_runner
 	cargo r -q --release bench $DIR/.. $2
 elif [[ $1 == "help" ]]; then
 	echo "Usage: $0 mode [FILES...]"
