37de6f41540736e8e59d108e71fe9be27343eb46
NDRAEY <andrejpavlenko666@gmail.com>
2025-07-09 00:59:36 +0900

    Simplify Clap parameters

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -12,29 +12,29 @@ struct CLI {
     #[arg(long = "gc-threshold", value_name = "gc_threshold")]
     gc_threshold: Option<usize>,
 
-    #[arg(long = "gc-debug", value_name = "gc_debug")]
-    gc_debug: Option<bool>,
+    #[arg(long = "gc-debug", default_value_t=false)]
+    gc_debug: bool,
 
-    #[arg(long = "ast-debug", value_name = "ast_debug")]
-    ast_debug: Option<bool>,
+    #[arg(long = "ast-debug", default_value_t=false)]
+    ast_debug: bool,
 
-    #[arg(long = "opcodes-debug", value_name = "opcodes_debug")]
-    opcodes_debug: Option<bool>,
+    #[arg(long = "opcodes-debug", default_value_t=false)]
+    opcodes_debug: bool,
 
-    #[arg(long = "lexer-debug", value_name = "lexer_debug")]
-    lexer_debug: Option<bool>,
+    #[arg(long = "lexer-debug", default_value_t=false)]
+    lexer_debug: bool,
 
-    #[arg(long = "parse-bench", value_name = "parse_bench")]
-    parse_bench: Option<bool>,
+    #[arg(long = "parse-bench", default_value_t=false)]
+    parse_bench: bool,
 
-    #[arg(long = "compile-bench", value_name = "compile_bench")]
-    compile_bench: Option<bool>,
+    #[arg(long = "compile-bench", default_value_t=false)]
+    compile_bench: bool,
 
-    #[arg(long = "lexer-bench", value_name = "lexer_bench")]
-    lexer_bench: Option<bool>,
+    #[arg(long = "lexer-bench", default_value_t=false)]
+    lexer_bench: bool,
 
-    #[arg(long = "runtime-bench", value_name = "runtime_bench")]
-    runtime_bench: Option<bool>,
+    #[arg(long = "runtime-bench", default_value_t=false)]
+    runtime_bench: bool,
 }
 
 pub unsafe fn cli() {
