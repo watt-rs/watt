8fd1afcc9ba5bee97bf3db3492b390ad3a0ebdfd
Вячеслав <slavlendstudio@gmail.com>
2025-07-08 20:48:18 +0500

    updated code style of fs lib. added is_directory fn.

--- src/vm/natives/libs/natives_fs.rs
+++ src/vm/natives/libs/natives_fs.rs
@@ -8,9 +8,10 @@ use crate::vm::table::Table;
 use crate::vm::values::Value;
 use crate::vm::vm::VM;
 use crate::{error, vm::natives::libs::utils};
-
 use std::io::{Read, Seek, Write};
+use crate::vm::flow::ControlFlow;
 
+// провайд
 pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Error> {
     // функции
     natives::provide(
@@ -19,63 +20,38 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
         1,
         "fs@open",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let filename = match vm.pop(&addr) {
-                Ok(Value::String(filename)) => &*filename,
-                Ok(a) => {
-                    error!(Error::own_text(
-                        addr.clone(),
-                        format!("Expected string, found {:?}", a),
-                        "check your code"
-                    ));
-                }
-                Err(_) => {
-                    todo!()
-                }
-            };
-
+            // имя файла
+            let filename = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
             // если надо пушить
             if should_push {
-                let file = match std::fs::OpenOptions::new().read(true).open(&filename) {
+                // файл
+                let file = match std::fs::OpenOptions::new().read(true).open(filename) {
                     Ok(file) => file,
-                    Err(e) => {
+                    Err(_) => {
                         vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
-
                         return Ok(());
                     }
                 };
-
+                // аллокация
                 let file = memory::alloc_value(file);
-
-                // добавляем
+                // пушим
                 vm.op_push(OpcodeValue::Raw(Value::Any(file)), table)?;
             }
             // успех
             Ok(())
         },
     );
-
     natives::provide(
         vm,
         built_in_address.clone(),
         1,
         "fs@create",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let filename = match vm.pop(&addr) {
-                Ok(Value::String(filename)) => &*filename,
-                Ok(a) => {
-                    error!(Error::own_text(
-                        addr.clone(),
-                        format!("Expected string, found {:?}", a),
-                        "check your code"
-                    ));
-                }
-                Err(_) => {
-                    todo!()
-                }
-            };
-
+            // имя файла
+            let filename = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
             // если надо пушить
             if should_push {
+                // файл
                 let file = match std::fs::OpenOptions::new()
                     .read(true)
                     .write(true)
@@ -89,9 +65,8 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
                         return Ok(());
                     }
                 };
-
+                // аллокация
                 let file = memory::alloc_value(file);
-
                 // добавляем
                 vm.op_push(OpcodeValue::Raw(Value::Any(file)), table)?;
             }
@@ -99,48 +74,36 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
             Ok(())
         },
     );
-
-    pub fn get_instance<'vm>(vm: &'vm mut VM, addr: &Address) -> &'vm mut std::fs::File {
-        match vm.pop(&addr) {
-            Ok(Value::Any(instance)) => {
-                let instance = unsafe { &mut *instance };
-
-                if !instance.is::<std::fs::File>() {
-                    error!(Error::own_text(
-                        addr.clone(),
-                        format!("Internal type in std.fs.File is not a Rust's `std::io::File`!"),
-                        "please file an issue at https://github.com/vyacheslavhere/watt"
-                    ));
-                }
-
-                instance.downcast_mut().unwrap()
-            }
-            Ok(a) => {
-                error!(Error::own_text(
-                    addr.clone(),
-                    format!("Expected instance, found {:?}", a),
-                    "check your code"
-                ));
-            }
-            Err(_) => {
-                todo!()
-            }
+    // pop файла из стека
+    pub unsafe fn pop_file<'vm>(vm: &'vm mut VM, addr: &Address) -> Result<&'vm mut std::fs::File, ControlFlow> {
+        // рав файл
+        let raw_file = utils::expect_any(addr.clone(), vm.pop(&addr)?, None);
+        // проверка, файл ли
+        if !(*raw_file).is::<std::fs::File>() {
+            error!(Error::new(
+                addr.clone(),
+                "internal type in std.fs.File is not a Rust's `std::io::File`!",
+                "please file an issue at https://github.com/vyacheslavhere/watt"
+            ));
         }
+        // возвращаем
+        Ok((*raw_file).downcast_mut().unwrap())
     }
-
+    // провайд
     natives::provide(
         vm,
         built_in_address.clone(),
         1,
         "fs@read_to_string",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let instance: &mut std::fs::File = get_instance(vm, &addr);
-
+            // получаем файл
+            let file: &mut std::fs::File = pop_file(vm, &addr)?;
             // если надо пушить
             if should_push {
+                // строка для чтения
                 let mut string = String::new();
-                instance.read_to_string(&mut string).unwrap();
-
+                file.read_to_string(&mut string).unwrap();
+                // пушим
                 vm.op_push(
                     OpcodeValue::Raw(Value::String(memory::alloc_value(string))),
                     table,
@@ -150,76 +113,70 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
             Ok(())
         },
     );
-
     natives::provide(
         vm,
         built_in_address.clone(),
         2,
         "fs@write",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // данные для записи
             let data = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
-            let instance: &mut std::fs::File = get_instance(vm, &addr);
-
+            // получаем файл
+            let file: &mut std::fs::File = pop_file(vm, &addr)?;
             // если надо пушить
             if should_push {
-                let value = instance.write(data.as_bytes());
-
+                // запись
+                let value = file.write(data.as_bytes());
+                // успешна ли запись
                 match value {
-                    Ok(_) => {
-                        vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
-                    }
-                    Err(e) => {
-                        vm.op_push(
-                            OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
-                            table,
-                        )?;
-                    }
+                    Ok(_) => { vm.op_push(OpcodeValue::Raw(Value::Null), table)?; }
+                    Err(e) => { vm.op_push(
+                        OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
+                        table,
+                    )?; }
                 }
             }
             // успех
             Ok(())
         },
     );
-
     natives::provide(
         vm,
         built_in_address.clone(),
         1,
         "fs@tell",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let instance: &mut std::fs::File = get_instance(vm, &addr);
-
+            // получаем файл
+            let file: &mut std::fs::File = pop_file(vm, &addr)?;
             // если надо пушить
             if should_push {
-                let value = instance.stream_position().unwrap_or(0);
-
+                let value = file.stream_position().unwrap_or(0);
                 vm.op_push(OpcodeValue::Int(value as _), table)?;
             }
             // успех
             Ok(())
         },
     );
-
     natives::provide(
         vm,
         built_in_address.clone(),
         3,
         "fs@seek",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // whence
             let whence = utils::expect_int(addr.clone(), vm.pop(&addr)?, None);
+            // позиция
             let position = utils::expect_int(addr.clone(), vm.pop(&addr)?, None);
-
-            let instance: &mut std::fs::File = get_instance(vm, &addr);
-
-            instance
+            // получаем файл
+            let file: &mut std::fs::File = pop_file(vm, &addr)?;
+            // seek
+            file
                 .seek(match whence {
                     1 => std::io::SeekFrom::Current(position as _),
                     2 => std::io::SeekFrom::End(position as _),
                     _ => std::io::SeekFrom::Start(position as _),
                 })
                 .unwrap();
-
             // если надо пушить
             if should_push {
                 vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
@@ -228,108 +185,25 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
             Ok(())
         },
     );
-
     natives::provide(
         vm,
         built_in_address.clone(),
         1,
         "fs@mkdir",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // имя директории
             let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
+            // создаём директорию
             let result = std::fs::create_dir(&name);
-
             // если надо пушить
             if should_push {
+                // успешно ли создана директория
                 if let Err(e) = result {
                     vm.op_push(
                         OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
                         table,
                     )?;
-                }
-
-                vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
-            }
-            // успех
-            Ok(())
-        },
-    );
-
-    natives::provide(
-        vm,
-        built_in_address.clone(),
-        1,
-        "fs@delete_directory",
-        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
-            let result = std::fs::remove_dir(name);
-
-            // если надо пушить
-            if should_push {
-                if let Err(e) = result {
-                    vm.op_push(
-                        OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
-                        table,
-                    )?;
-                }
-
-                vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
-            }
-            // успех
-            Ok(())
-        },
-    );
-
-    natives::provide(
-        vm,
-        built_in_address.clone(),
-        1,
-        "fs@delete_directory_all",
-        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
-            let result = std::fs::remove_dir_all(name);
-
-            // если надо пушить
-            if should_push {
-                if let Err(e) = result {
-                    vm.op_push(
-                        OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
-                        table,
-                    )?;
-                }
-
-                vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
-            }
-            // успех
-            Ok(())
-        },
-    );
-
-    natives::provide(
-        vm,
-        built_in_address.clone(),
-        1,
-        "fs@exists",
-        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
-            let result = std::fs::exists(name);
-
-            // если надо пушить
-            if should_push {
-                // if let Err(e) = result {
-                //     vm.op_push(
-                //         OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _)),
-                //         table,
-                //     )?;
-                // }
-
-                if let Ok(data) = result {
-                    vm.op_push(OpcodeValue::Raw(Value::Bool(data)), table)?;
                 } else {
-                    // TODO: Change it when I learn to use typeof (return errno)
                     vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
                 }
             }
@@ -337,39 +211,110 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
             Ok(())
         },
     );
-
+    natives::provide(
+        vm,
+        built_in_address.clone(),
+        1,
+        "fs@delete_directory",
+        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // имя директории
+            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
+            // удаляем директорию
+            let result = std::fs::remove_dir(name);
+            // если надо пушить
+            if should_push {
+                // успешно ли удалена директория
+                if let Err(e) = result {
+                    vm.op_push(
+                        OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
+                        table,
+                    )?;
+                } else {
+                    vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
+                }
+            }
+            // успех
+            Ok(())
+        },
+    );
+    natives::provide(
+        vm,
+        built_in_address.clone(),
+        1,
+        "fs@delete_directory_all",
+        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // имя директории
+            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
+            // удаляем древо директории
+            let result = std::fs::remove_dir_all(name);
+            // если надо пушить
+            if should_push {
+                // успешно ли удалено древо директории
+                if let Err(e) = result {
+                    vm.op_push(
+                        OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _),
+                        table,
+                    )?;
+                } else {
+                    vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
+                }
+            }
+            // успех
+            Ok(())
+        },
+    );
+    natives::provide(
+        vm,
+        built_in_address.clone(),
+        1,
+        "fs@exists",
+        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // путь
+            let path = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
+            // проверка на существование пути
+            let result = std::fs::exists(path);
+            // если надо пушить
+            if should_push {
+                if let Ok(data) = result {
+                    vm.op_push(OpcodeValue::Raw(Value::Bool(data)), table)?;
+                } else {
+                    // NDRAEY todo: Change it when I learn to use typeof (return errno)
+                    vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
+                }
+            }
+            // успех
+            Ok(())
+        },
+    );
     natives::provide(
         vm,
         built_in_address.clone(),
         1,
         "fs@list",
         |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
-            let name = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
-
+            // путь
+            let path = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
             // если надо пушить
             if should_push {
-                let result = std::fs::read_dir(name);
-
+                // читаем директорию
+                let result = std::fs::read_dir(path);
+                // создаем список
                 match result {
-                    Err(_e) => {
-                        // vm.op_push(
-                        //     OpcodeValue::Int(e.raw_os_error().unwrap_or(0) as _)),
-                        //     table,
-                        // )?;
-
-                        // TODO: Change it when I learn to use typeof (return errno)
+                    Err(_) => {
+                        // NDRAEY todo: Change it when I learn to use typeof (return errno)
                         vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
                     }
                     Ok(data) => {
+                        // список
                         let paths: Vec<Value> = data
                             .filter(Result::is_ok)
                             .map(|x| x.unwrap().path().to_string_lossy().to_string())
                             .map(|st| Value::String(memory::alloc_value(st)))
                             .collect();
-
-                        let watt_paths = Value::List(memory::alloc_value(paths));
-
-                        vm.op_push(OpcodeValue::Raw(watt_paths), table)?;
+                        // аллокация
+                        let allocated = Value::List(memory::alloc_value(paths));
+                        // пушим
+                        vm.op_push(OpcodeValue::Raw(allocated), table)?;
                     }
                 }
             }
@@ -377,7 +322,33 @@ pub unsafe fn provide(built_in_address: Address, vm: &mut VM) -> Result<(), Erro
             Ok(())
         },
     );
-
+    natives::provide(
+        vm,
+        built_in_address.clone(),
+        1,
+        "fs@is_directory",
+        |vm: &mut VM, addr: Address, should_push: bool, table: *mut Table| {
+            // путь
+            let path = &*utils::expect_string(addr.clone(), vm.pop(&addr)?, None);
+            // если надо пушить
+            if should_push {
+                // читаем директорию
+                let result = std::fs::metadata(path);
+                // создаем список
+                match result {
+                    Err(_) => {
+                        vm.op_push(OpcodeValue::Raw(Value::Null), table)?;
+                    }
+                    Ok(data) => {
+                        // пушим
+                        vm.op_push(OpcodeValue::Bool(data.is_dir()), table)?;
+                    }
+                }
+            }
+            // успех
+            Ok(())
+        }
+    );
     // успех
     Ok(())
 }
