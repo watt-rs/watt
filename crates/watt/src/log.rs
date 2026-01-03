81f5b2816f0f7783a2a819c4dbf170d6cf2f2326
Andrey <pikachu_andrey@vk.com>
2026-01-03 23:14:13 +0900

    Log when needed

--- crates/watt/src/log.rs
+++ crates/watt/src/log.rs
@@ -1,16 +1,17 @@
 /// Imports
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
 
 /// Initializes logging
 pub fn init() {
-    tracing_subscriber::fmt()
-        .with_span_events(FmtSpan::ENTER)
-        .with_target(false)
-        .with_level(true)
-        .with_line_number(true)
-        .with_max_level(LevelFilter::TRACE)
-        .pretty()
-        .compact()
-        .init();
+    if let Ok(log) = std::env::var("RUST_LOG") {
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::new(&format!("watt={log}")))
+            .with_span_events(FmtSpan::ENTER)
+            .with_target(false)
+            .with_level(true)
+            .with_line_number(true)
+            .pretty()
+            .compact()
+            .init();
+    }
 }
